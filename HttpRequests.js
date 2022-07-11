df6aae3eef8312ec545dc76f27166f6f52b565c4
Scripter-Automation <96034633+Scripter-Automation@users.noreply.github.com>
2022-07-10 22:51:55 -0500

    Update HttpRequests.js

--- HttpRequests.js
+++ HttpRequests.js
@@ -13,7 +13,7 @@ function doGet(e){
         .setFormula(`=Query(${table}!1:${Main.getLastRow()}, ${queryString},1)`);
     }
 
-    res = ParseSheet(DataBase, "Main");
+    res = ParseSheet("1buJZDU3yvbx8-yuQKuzXQDPX4ivD6M0kE8rEvnXJ0a4", "Main");
 
     
 
@@ -26,13 +26,26 @@ function doPost(e){
     let res;
     const req = JSON.parse(body);
 
+
+    const APIKey = req.APIKey
+    
+    if(!parseAPIKey(APIKey)){
+
+        res = {error, msg: "APIKey no es correcto"}
+        
+        return ContentService.createTextOutput(JSON.stringify(res)).setMimeType(ContentService.MimeType.JSON);
+    }
+
+
     const PrimaryKey = req.primaryKey
     const DataBase = ParseDatabaseSelector(req.dataBase)
+    const DataBaseName = req.dataBase
     const command = req.command
     const table = req.table
     const data = req.data
     const column = req.col
     const headers = req.headers
+    const costumFunction = req.costumFunction
 
     
     switch(command){
@@ -40,22 +53,16 @@ function doPost(e){
 
             res = createRow(DataBase ,table,data)
 
-            return res
-
         break;
 
         case "updateValue":
 
             res = updateValue(DataBase, table, PrimaryKey, data, column)
             
-            return res
-
         break;
 
         case "updateRow":
             res = updateRow(DataBase,table, PrimaryKey, data)
-
-            return res
 
         break;
 
@@ -63,23 +70,38 @@ function doPost(e){
 
             res = dropRow(DataBase, table, PrimaryKey)
 
-            return res
-
         break;
         case "createTable":
 
             res = createTable(DataBase, table, headers)
-
-            return res
 
         break;
         case "dropTable":
 
             res = dropTable(DataBase, table)
 
-            return res
+        break;
+        case "costumFunction":
+
+            res = costumFunction()
 
         break;
+        case "createDatabase":
+
+        res = CreateDatabase(DataBaseName)
+
+        break;
+        case "DropDatabase":
+        
+        res = DropDatabase(DataBaseName)
+
+        break;
+
+
+
     }
+
+    
+    return ContentService.createTextOutput(JSON.stringify(res)).setMimeType(ContentService.MimeType.JSON);
 
 }
